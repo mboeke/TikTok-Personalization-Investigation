80708f39c919837fa1ac9afa981b0c79b0aa3492
mboeke <maximilian.boeker@uzh.ch>
2022-10-18 14:29:58 +0200

    Added some small comments

--- Testing/ParalleliseTesting.py
+++ Testing/ParalleliseTesting.py
@@ -14,38 +14,41 @@ def get_test_data():
         test_json = json.load(file)
 
     # test_data with settings from database
-    # test_data = []
-    # for test_set in test_json:
-    #     if test_json.get(test_set).get('login'):
-    #         test_json.get(test_set)['phone_number'] = database.get_phone_number(test_user_id=test_json.get(
-    #             test_set).get('test_user_id'))
-    #         test_json.get(test_set)['country_phone_number_prefix'] = database.get_country_phone_number_prefix(
-    #             test_user_id=test_json.get(test_set).get('test_user_id'))
-    #     test_json.get(test_set)['proxy'] = {
-    #         "proxy_username": "PLACEHOLDER", "proxy_password": "PLACEHOLDER",
-    #         "proxy_host": database.get_proxy_host(test_user_id=test_json.get(test_set).get('test_user_id')),
-    #         "proxy_port": database.get_proxy_port(test_user_id=test_json.get(test_set).get('test_user_id')),
-    #         "country": database.get_proxy_country(test_user_id=test_json.get(test_set).get('test_user_id'))}
-    #     test_data.append(test_json.get(test_set))
-    # return test_data
-
-    # test test setting data
-    test_data = [test_json.get('167')]
-    for test_set in test_data:
-        if test_set.get('login'):
-            test_set['phone_number'] = database.get_phone_number(test_user_id=test_set.get('test_user_id'))
-            test_set['country_phone_number_prefix'] = database.get_country_phone_number_prefix(
-                test_user_id=test_set.get('test_user_id'))
-        test_set['proxy'] = {
+    # code obtains all details from json & database to initiliaze test
+    test_data = []
+    for test_set in test_json:
+        if test_json.get(test_set).get('login'):
+            test_json.get(test_set)['phone_number'] = database.get_phone_number(test_user_id=test_json.get(
+                test_set).get('test_user_id'))
+            test_json.get(test_set)['country_phone_number_prefix'] = database.get_country_phone_number_prefix(
+                test_user_id=test_json.get(test_set).get('test_user_id'))
+        test_json.get(test_set)['proxy'] = {
             "proxy_username": "PLACEHOLDER", "proxy_password": "PLACEHOLDER",
-            "proxy_host": database.get_proxy_host(test_user_id=167),
-            "proxy_port": database.get_proxy_port(test_user_id=167),
-            "country": database.get_proxy_country(test_user_id=167)
-        }
-        # c1 = 'US'
-        # test_set['proxy'] = {"proxy_username": "PLACEHOLDER", "proxy_password": "PLACEHOLDER",
-        #                      'proxy_host': 'PLACEHOLDER', 'proxy_port': 'PLACEHOLDER', 'country': c1}
+            "proxy_host": database.get_proxy_host(test_user_id=test_json.get(test_set).get('test_user_id')),
+            "proxy_port": database.get_proxy_port(test_user_id=test_json.get(test_set).get('test_user_id')),
+            "country": database.get_proxy_country(test_user_id=test_json.get(test_set).get('test_user_id'))}
+        test_data.append(test_json.get(test_set))
     return test_data
+
+    # test the test's setting data
+    # use this code section to initialize chrome session and login newly created user for the first time, to then
+    # manually complete the registration process
+    # test_data = [test_json.get('167')]
+    # for test_set in test_data:
+    #     if test_set.get('login'):
+    #         test_set['phone_number'] = database.get_phone_number(test_user_id=test_set.get('test_user_id'))
+    #         test_set['country_phone_number_prefix'] = database.get_country_phone_number_prefix(
+    #             test_user_id=test_set.get('test_user_id'))
+    #     test_set['proxy'] = {
+    #         "proxy_username": "PLACEHOLDER", "proxy_password": "PLACEHOLDER",
+    #         "proxy_host": database.get_proxy_host(test_user_id=167),
+    #         "proxy_port": database.get_proxy_port(test_user_id=167),
+    #         "country": database.get_proxy_country(test_user_id=167)
+    #     }
+    #     # c1 = 'US'
+    #     # test_set['proxy'] = {"proxy_username": "PLACEHOLDER", "proxy_password": "PLACEHOLDER",
+    #     #                      'proxy_host': 'PLACEHOLDER', 'proxy_port': 'PLACEHOLDER', 'country': c1}
+    # return test_data
 
     # for account creation purposes:
     # cur = [test_data[0]]
@@ -53,6 +56,7 @@ def get_test_data():
     # return cur
 
 
+# main function initializing all different steps within one test iteration
 def run_test(test_data):
     # setting start time
     start = time.time()
@@ -95,7 +99,7 @@ def run_test(test_data):
         helper.handle_banners()
         helper.pause_video()
 
-    # set cookies
+    # set cookies if applicable
     if test_data.get('reuse_cookies'):
         helper.set_cookies()
 
@@ -136,9 +140,10 @@ def run_test(test_data):
                                         posts_with_hashtag_to_watch_longer=test_data.get('posts_with_hashtag_to_watch_longer'),
                                         posts_of_content_creators_to_like=test_data.get('posts_of_content_creators_to_like'),
                                         posts_of_music_ids_to_like=test_data.get('posts_of_music_ids_to_like'))
-    helper.close_driver()
+
     # commencing shut down of test run: unflagging used proxy, closing driver, storing collected data, computing
     # duration and storing it for corresponding testrun
+    helper.close_driver()
     helper.database.unflag_proxy(proxy_host=test_data['proxy']['proxy_host'],
                                  proxy_port=test_data['proxy']['proxy_port'])
     data_storing.store_collected_data()
@@ -151,6 +156,8 @@ def run_test(test_data):
 
 if __name__ == '__main__':
     tests = get_test_data()
+
+    # crete test run object with given test data and run tests in parallel
     with TestRun(test_data=tests) as test_run:
         for test in tests:
             test['test_run_id'] = test_run.test_run_id
@@ -158,6 +165,8 @@ if __name__ == '__main__':
             test_data_results = executor.map(run_test, tests)
         test_user_ids = []
         batch_size = 0
+
+        # obtain test results and store them accordingly
         for test in test_data_results:
             test_run.store_test_duration(duration=test.get('duration'), test_user_id=test.get('test_user_id'))
             test_user_ids.append(test.get('test_user_id'))
